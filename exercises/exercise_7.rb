b44ae5cb88852b6aba25074a237a28c9ad30b846
Anastasia <z.anastasia11@gmail.com>
2023-07-27 18:36:38 -0700

    Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,22 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# VALIDATION
+
+# Ask the user for a store name
+puts "Enter the store name:"
+store_name = gets.chomp
+
+# Display the store name entered by the user
+puts "You entered: #{store_name}"
+
+store = Store.create(name: store_name)
+
+if store.save
+  puts "Store created successfully"
+else 
+  puts " Errors occured while creating the store"
+  store.errors.full_messages.each do |error_message|
+    puts error_message
+  end
+end  
