9ac3b27eb5138a66561fa5e4c3486e60b56ea9c7
Anastasia <z.anastasia11@gmail.com>
2023-07-27 18:12:07 -0700

    add amployees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,17 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# One-to-many association
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
+@store1.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 50)
+@store1.employees.create(first_name: "Michael", last_name: "Johnson", hourly_rate: 55)
+@store1.employees.create(first_name: "Emily", last_name: "Williams", hourly_rate: 65)
+@store1.employees.create(first_name: "David", last_name: "Brown", hourly_rate: 45)
+
+@store2.employees.create(first_name: "Alex", last_name: "Miller", hourly_rate: 55)
+@store2.employees.create(first_name: "Olivia", last_name: "Davis", hourly_rate: 50)
+@store2.employees.create(first_name: "Ethan", last_name: "Wilson", hourly_rate: 60)
+@store2.employees.create(first_name: "Sophia", last_name: "Lee", hourly_rate: 65)
+@store2.employees.create(first_name: "Aiden", last_name: "Taylor", hourly_rate: 45)
+@store2.employees.create(first_name: "Mia", last_name: "Johnson", hourly_rate: 55)
